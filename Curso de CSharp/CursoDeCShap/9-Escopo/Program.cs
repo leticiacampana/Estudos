c75ffc46f028279328cc6f4286a2b23a55716486
Letícia Campana <88500045+leticiacampana@users.noreply.github.com>
2021-10-21 18:09:47 -0300

    Atribuições de variáveis e Condicionais

---
+++ Curso de CSharp/CursoDeCShap/9-Escopo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto  - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+
+                Console.WriteLine(mensagemAdicional);
+
+            }
+            else
+                mensagemAdicional = "João NÂO está acompanhado";
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+
+            }
+            else
+            {
+                Console.WriteLine(" Não pode entrar");
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Curso de CSharp/CursoDeCShap/P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11 - Calcula poupança 2");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine(" Após " + contadorMes + "meses, você terá R$" + valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
