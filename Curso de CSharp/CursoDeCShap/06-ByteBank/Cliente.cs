ed6f8c08fc4681d4e12e0cd3d51fe46b3b9fb809
Letícia Campana <88500045+leticiacampana@users.noreply.github.com>
2021-10-27 16:23:38 -0300

    Orientação Objetos

---
+++ Curso de CSharp/CursoDeCShap/06-ByteBank/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class Cliente
+    {
+        private string _cpf;
+        public string Nome { get; set; }
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+
+            }
+            set
+            {
+                //Escrevo minha lógica da validação de CPF
+                _cpf = value;
+            }
+
+
+
+
+          
+            
+        } 
+
+
+        
+
+        
+    
+
+
+       
+        public string Profissão { get; set; }
+
+    }
+}
---
+++ Curso de CSharp/CursoDeCShap/06-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using _07_ByteBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Letícia";
+            cliente.CPF = "434.564.879-20";
+            cliente.Profissão = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
